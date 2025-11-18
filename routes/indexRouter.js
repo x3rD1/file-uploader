aa734d97a5542966e16cd64c54779b2e3fdb531c
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-17 20:21:32 -0800

    Remove isAuth

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,10 +1,7 @@
 const { Router } = require("express");
 const indexRouter = Router();
 const indexController = require("../controllers/indexControllers");
-const { isAuth } = require("../middlewares/auth");
+
 indexRouter.get("/", indexController.getIndexPage);
-indexRouter.get("/welcome", isAuth, (req, res) => {
-  res.render("welcome");
-});
 
 module.exports = indexRouter;
