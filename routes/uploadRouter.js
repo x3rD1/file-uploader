f7543ee4f5b8f15900d9d365af9a806adf4de509
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-18 17:35:50 -0800

    Add routes for folder CRUD

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,34 +1,21 @@
 const { Router } = require("express");
 const uploadRouter = Router();
 const uploadController = require("../controllers/uploadController");
+const folderController = require("../controllers/folderCRUDController");
+const fileController = require("../controllers/fileCRUDController");
 const { isAuth } = require("../middlewares/auth");
-
-const fs = require("fs");
-const path = require("path");
-const multer = require("multer");
-const uploadDir = path.join(__dirname, "../uploads");
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const base = path.basename(file.originalname, ext);
-
-    let filename = file.originalname;
-    let counter = 1;
-
-    while (fs.existsSync(path.join(uploadDir, filename))) {
-      filename = `${base} (${counter})${ext}`;
-      counter++;
-    }
-
-    cb(null, filename);
-  },
-});
-const upload = multer({ storage });
+const upload = require("../config/multer");
+const { folder } = require("../db/prisma");
 
 uploadRouter.get("/", isAuth, uploadController.getUploadPage);
-uploadRouter.post("/", upload.single("file"), uploadController.uploadFile);
+uploadRouter.post("/folders/create", isAuth, folderController.createFolder);
+uploadRouter.post("/folders/delete/:id", isAuth, folderController.deleteFolder);
+uploadRouter.post("/folders/rename/:id", isAuth, folderController.renameFolder);
+uploadRouter.post(
+  "/files/upload",
+  isAuth,
+  upload.single("file"),
+  fileController.uploadFile
+);
 
 module.exports = uploadRouter;
