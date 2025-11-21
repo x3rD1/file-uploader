ea63dcf559a6848808ec828313d117b2fff9aa4a
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-20 23:47:12 -0800

    Move in getFolderPage function from uploadController to folderCRUDController

--- controllers/folderCRUDController.js
+++ controllers/folderCRUDController.js
@@ -1,5 +1,16 @@
 const prisma = require("../db/prisma");
 
+exports.getFolderPage = async (req, res) => {
+  const folderId = parseInt(req.params.id, 10);
+  const files = await prisma.file.findMany({
+    where: { folderId },
+  });
+
+  res.locals.files = files;
+  res.locals.folderId = folderId;
+  res.render("folder");
+};
+
 exports.createFolder = async (req, res) => {
   const { name } = req.body;
   await prisma.folder.create({
