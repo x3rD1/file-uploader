75ed6987a90200be11b1e9ab2a06b8202bcc2fbe
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-21 20:37:39 -0800

    Add supabase config

---
+++ config/supabase.js
@@ -0,0 +1,10 @@
+// supabase.js
+require("dotenv").config();
+const { createClient } = require("@supabase/supabase-js");
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseServiceRole = process.env.SUPABASE_SERVICE_ROLE;
+
+const supabase = createClient(supabaseUrl, supabaseServiceRole);
+
+module.exports = supabase;
