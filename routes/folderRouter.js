de30d6b38c07f8a98ff62a596edde93917ae79fa
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-23 19:18:44 -0800

    Add get routes for shared folder

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -5,6 +5,16 @@ const { isAuth } = require("../middlewares/auth");
 
 folderRouter.get("/:id", isAuth, folderController.getFolderPage);
 folderRouter.post("/create", isAuth, folderController.createFolder);
+folderRouter.get("/share/:shareId", folderController.viewSharedFolder);
+folderRouter.get(
+  "/share/:shareId/folder/:folderId",
+  folderController.viewSharedSubfolder
+);
+folderRouter.get(
+  "/share/download/:fileId",
+  folderController.downloadSharedFile
+);
+
 folderRouter.post("/:id/createSub", isAuth, folderController.createSubFolder);
 folderRouter.post("/:id/rename", isAuth, folderController.renameFolder);
 folderRouter.post("/:id/delete", isAuth, folderController.deleteFolder);
